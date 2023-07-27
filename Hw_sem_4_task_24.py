b2a965923a5365b92ead6a027008bea304155ae3
Pradim22 <792616@mail.ru>
2023-07-27 21:35:19 +0530

    task 24 is ready

--- Hw_sem_4_task_24.py
+++ Hw_sem_4_task_24.py
@@ -1,6 +1,7 @@
-'''
+"""
+Задача 24
 черника растет на круглой грядке, причем кусты высажены только по окружности. 
-Таким образом, укаждого куста есть ровно два соседних. 
+Таким образом, у каждого куста есть ровно два соседних. 
 Всего на грядке растет  N  кустов.
 Эти кусты обладают разной урожайностью, поэтому ко времени сбора на них
 выросло различное число ягод – на i-ом кусте выросло ai ягод.
@@ -10,11 +11,30 @@ Cистема сбора состоит из управляющего модул
 Напишите программу для нахождения максимального числа ягод, которое может
 собрать за один заход собирающий модуль, находясь перед некоторым кустом
 заданной во входном списке урожайности грядки.
-'''
+"""
 import random
 
-amount_n = int(input("введите количество кустов: "))
-if amount_n > 0:
-    
-
-
+amount_n = int(input("введите количество кустов на грядке: "))
+result = 0
+if amount_n > 2:
+    berry_list = []
+    for i in range(amount_n):
+        berry_list.append(random.randint(10, 20))
+    print(berry_list)
+    count = 0
+    for i in range(amount_n):
+        if i == 0:
+            count = berry_list[i] + berry_list[i + 1] + berry_list[amount_n - 1]
+            if count > result:
+                result = count
+        elif i == amount_n - 1:
+            count = berry_list[i] + berry_list[i - 1] + berry_list[0]
+            if count > result:
+                result = count
+        else:
+            count = berry_list[i] + berry_list[i + 1] + berry_list[i - 1]
+            if count > result:
+                result = count
+    print(result)
+else:
+    print("введите количество кустов не менее 3")
