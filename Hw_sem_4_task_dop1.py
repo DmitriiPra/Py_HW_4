bf13741ba83c1c968c7650f301f3f35c11f1c09f
Pradim22 <792616@mail.ru>
2023-08-01 06:40:59 +0530

    Task dop1 is ready

--- Hw_sem_4_task_dop1.py
+++ Hw_sem_4_task_dop1.py
@@ -9,3 +9,26 @@
 Используйте функции
 """
 
+
+def converter(decimal_input: int, n: int) -> str:
+    converter_number: str = ""
+    while decimal_input > 0:
+        remainder: int = decimal_input % n
+        converter_number = str(remainder) + converter_number
+        decimal_input //= n
+    return converter_number
+
+
+
+
+decimal_input = int(input("Введите число в десятеричной системе: "))
+
+if decimal_input < 0:
+    print("Число должно быть неотрицательным.")
+
+else:
+    binary_output = converter(decimal_input, 2)
+    print(f"В двоичной системе: {binary_output}")
+
+    octal_output = converter(decimal_input, 8)
+    print(f"В восьмеричной системе: {octal_output}")
